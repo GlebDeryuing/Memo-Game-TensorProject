38cc0ed973cece489f0951ee836807e7e76c04c7
VladimirSivarev <58487411+VladimirSivarev@users.noreply.github.com>
2019-12-09 05:47:46 +0300

    added a checking of repeating generated elememnts

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 (function(){
     document.addEventListener("DOMContentLoaded", function(){
         let playingCards = [];
+        let valueToBeChecked;
 
         function randomiser(min, max){
             let random = min + Math.random() * (max - min);
             return Math.round(random);
-        }
-        
+        }   
         function shuffleForMatrix(matrix){
             for(let i = 0; i < 4; i++){
                 for(let j = 0; j < 4; j++){
@@ -21,18 +21,24 @@
         for (let i = 0; i < 4; i++){
             playingCards[i] = [];
         }
-        for (let i = 0; i < 2; i++){
+        for (let i = 0; i < 4; i++){
             for (let j = 0; j < 4; j++){
-                playingCards[i][j] = randomiser(0, 49);
+                valueToBeChecked = randomiser(0, 49);
+                while (playingCards.indexOf(valueToBeChecked) != -1) {
+                    valueToBeChecked = randomiser(0, 49);
+                }
+                playingCards[i][j] = valueToBeChecked;
             }
         }
-
         for (let i = 2; i < 4; i++){
             for (let j = 0; j < 4; j++){
                 playingCards[i][j] = playingCards[i - 2][j];
             }
         }
-
+        //максимально возможная перетасовка элементов
+        for (let i = 0; i < 4; i++) {
+            playingCards[i] = shuffleForMatrix(playingCards[i]);    
+        }
         playingCards = shuffleForMatrix(playingCards);
         console.log(playingCards)
     })
