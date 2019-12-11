3be4f8d3a3c7b26ead0d42fe1ae875486c4b69c3
Дерюгин Глеб <deryugin.gleb@gmail.com>
2019-12-11 18:59:39 +0300

    Логика игры (новая)

--- script.js
+++ script.js
@@ -1,46 +1,40 @@
-(function(){
-    document.addEventListener("DOMContentLoaded", function(){
-        let playingCards = [];
-        let valueToBeChecked;
+var playingCards = [],
+    freeCounter = 0;
 
-        function randomiser(min, max){
-            let random = min + Math.random() * (max - min);
-            return Math.round(random);
-        }   
-        function shuffleForMatrix(matrix){
-            for(let i = 0; i < 16; i++){
-                for(let j = 0; j < 16; j++){
-                    let key = Math.floor(Math.random() * (i + 1));
-                    let temporary = matrix[i];
-                    matrix[i] = matrix[key];
-                    matrix[key] = temporary;
-                }
-            }
-            return matrix;
-        }
-        function checkingForEquality(matrix, firstCard, secondCard){
-            if (matrix[firstCard] === matrix[secondCard]) {
-                return true
-            } else {
-                return false
-            }
-        }
+function objectCreation(value) {
+    var newobj = {
+        value: value,
+        passed: false
+    }
+    return newobj;
 
-        for (let i = 0; i < 16; i++){
-            valueToBeChecked = randomiser(0, 49);
-            while (playingCards.indexOf(valueToBeChecked) != -1) {
-                valueToBeChecked = randomiser(0, 49);
-            }
-            playingCards[i] = valueToBeChecked;
+}
+
+function generator(count) {
+    playingCards = [];
+    freeCounter = count;
+    for (let index = 0; index < count / 2; index++) {
+        playingCards.push(objectCreation(index));
+        playingCards.push(objectCreation(index));
+    }
+    for (let index = 0; index < playingCards.length; index++) {
+        var tempObj = playingCards[index],
+            newRandom = Math.floor(Math.random() * (count - 1));
+        playingCards[index] = playingCards[newRandom];
+        playingCards[newRandom] = tempObj;
+    }
+}
+
+function check(a, b) { // index, потом див
+    var first = playingCards[a],
+        second = playingCards[b];
+    if (!first.passed && !second.passed && first.value === second.value) {
+        freeCounter -= 2;
+        first.passed = true;
+        second.passed = true;
+        if (freeCounter === 0) {
+            console.log('win')
         }
-        for (let i = 8; i < 16; i++){
-            playingCards[i] = playingCards[i - 8];
-        }
-        //максимально возможная перетасовка элементов
-        for (let i = 0; i < 4; i++) {
-            playingCards[i] = shuffleForMatrix(playingCards[i]);    
-        }
-        playingCards = shuffleForMatrix(playingCards);
-        console.log(playingCards)
-    })
-})()
+        return true;
+    } else return false;
+}
