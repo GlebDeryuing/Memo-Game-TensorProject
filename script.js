62a1ed2944b29de21a811fb8a2f86ad6db77059d
Дерюгин Глеб <deryugin.gleb@gmail.com>
2019-12-12 12:33:12 +0300

    Работающая игра на странице
    запуск новой игры через generator(count)

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+var memo,
+    selectedId = -1,
+    playingDivs = [];
+
+document.addEventListener('DOMContentLoaded', function() {
+    memo = document.body.querySelector('.memo');
+    generator(36);
+    memo.addEventListener('click', function(e) {
+        var targ = e.target.nodeName === "DIV" ? e.target : e.target.parentNode;
+        if (targ.id != "" && targ.className != "selected" && targ.className != "defeated" && targ.className != "memo") {
+            targ.className = "selected";
+            var img = document.createElement("img");
+            img.src = "image/cards/" + targ.id + ".svg"; //id заменить на номер картинки
+            targ.appendChild(img);
+            if (selectedId === -1) {
+                selectedId = playingDivs.indexOf(targ);
+            } else {
+                same = check(selectedId, playingDivs.indexOf(targ));
+                lastTarg = playingDivs[selectedId];
+                selectedId = -1;
+                setTimeout(() => {
+                    if (same) {
+                        targ.className = "defeated";
+                        lastTarg.className = "defeated";
+                    } else {
+                        targ.className = "";
+                        targ.innerHTML = "";
+                        lastTarg.className = "";
+                        lastTarg.innerHTML = "";
+                    }
+                }, 300);
+
+            }
+        } else if (targ.className === "selected") {
+            targ.className = "";
+            targ.innerHTML = "";
+            selectedId = -1;
+        }
+    });
+});
+
 var playingCards = [],
     freeCounter = 0;
 
@@ -12,6 +53,7 @@ function objectCreation(value) {
 
 function generator(count) {
     playingCards = [];
+    playingDivs = [];
     freeCounter = count;
     for (let index = 0; index < count / 2; index++) {
         playingCards.push(objectCreation(index));
@@ -23,6 +65,16 @@ function generator(count) {
         playingCards[index] = playingCards[newRandom];
         playingCards[newRandom] = tempObj;
     }
+    memo.innerHTML = '';
+    for (let index = 0; index < playingCards.length; index++) {
+        var field = document.createElement('div');
+        field.className = "";
+        field.id = playingCards[index].value;
+        playingDivs.push(field);
+        memo.appendChild(field);
+    }
+    resize();
+
 }
 
 function check(a, b) { // index, потом див
@@ -37,4 +89,13 @@ function check(a, b) { // index, потом див
         }
         return true;
     } else return false;
+}
+
+function resize() {
+    var blocks = memo.children,
+        size = 100 / Math.ceil(Math.sqrt(blocks.length));
+    for (let index = 0; index < blocks.length; index++) {
+        blocks[index].style.height = "calc(" + size + "% - 8px)";
+        blocks[index].style.width = "calc(" + size + "% - 8px)";
+    };
 }
