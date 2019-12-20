a6a5408fea012266473f51abeb980b24ecdaa56b
Andrew <52471156+ankoz2000@users.noreply.github.com>
2019-12-20 18:31:11 +0300

    fix

--- chat.js
+++ chat.js
@@ -6,13 +6,16 @@ export default function sent() {
     .set('Content-Type', 'application/json')
     .send({ name: 'lol' })
     .then(console.log('ready!'))
+    .then((res) => {
+      console.log(res.body.sid);
+    })
     .catch((err) => console.log(err));
 
-  request
+  /*request
     .get('/api/users')
     .set('Content-Type', 'application/json')
     .then(res => {
       const users = new json.JSONObject(res);
       console.log(users);
-    });
+    });*/
 }
